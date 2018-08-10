93cb870f1896ca86b3a322e487e69fb5acddb5f6
damien <damien.savoldelli@gmail.com>
2018-08-10 20:58:31 +0200

    Begin app

---
+++ app/Admin/AddPost/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export class AddPost extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Add a post </Text>
+      </View>
+    );
+  }
+}
+
+export default AddPost;
