59652e78ee0a5b01f9f6ee00ee6f9e24a0e023de
damien <damien.savoldelli@gmail.com>
2018-08-10 20:54:12 +0200

    creates indexes for ios & android

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-AppRegistry.registerComponent('sell_it_app', () => App);
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+// AppRegistry.registerComponent('sell_it_app', () => App);
