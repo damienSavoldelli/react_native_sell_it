f7328db76f9b6504163830846642c048056a1c2e
damien <damien.savoldelli@gmail.com>
2018-08-10 21:42:28 +0200

    fix conflict on App

--- App.js
+++ App.js
@@ -1,18 +1,5 @@
 import { Navigation } from 'react-native-navigation';
 
-<<<<<<< HEAD
-import Login from './app/Login';
-import Home from './app/Home';
-import AddPost from './app/Admin/AddPost';
-
-Navigation.registerComponent('sellitApp.Login', () => Login);
-Navigation.registerComponent('sellitApp.Home', () => Home);
-Navigation.registerComponent('sellitApp.AddPost', () => AddPost);
-
-export default () => Navigation.startSingleScreenApp({
-  screen: {
-    screen: 'sellitApp.Login',
-=======
 import Home from './app/Home';
 import Login from './app/Login';
 import AddPost from './app/Admin/AddPost';
@@ -25,7 +12,6 @@ Navigation.registerComponent('sell_it_app.AddPost', () => AddPost);
 export default () => Navigation.startSingleScreenApp({
   screen: {
     screen: 'sell_it_app.Login',
->>>>>>> gitignore
     title: 'Login',
     navigatorStyle: {
       navBarHidden: true,
