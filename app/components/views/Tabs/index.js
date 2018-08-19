ebe2531ecef6f7225cf1d758c6039e71ed6571c7
damien <damien.savoldelli@gmail.com>
2018-08-19 22:34:41 +0200

    update tab menu add font awseom icone ; with promise

--- app/components/views/Tabs/index.js
+++ app/components/views/Tabs/index.js
@@ -1,23 +1,31 @@
 import { Navigation } from 'react-native-navigation';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 import FalseIcon from '../../../assets/images/circle.png';
 
 const LoadTabs = () => {
-  Navigation.startTabBasedApp({
-    tabs: [
-      {
-        screen: 'sell_it_app.Home',
-        label: 'Home',
-        title: 'Home',
-        icon: FalseIcon,
-      },
-      {
-        screen: 'sell_it_app.AddPost',
-        label: 'Sell it',
-        title: 'Sell it',
-        icon: FalseIcon,
-      },
-    ],
+  Promise.all([
+    Icon.getImageSource('bars', 20, 'white'),
+    Icon.getImageSource('dollar', 20, 'white'),
+    Icon.getImageSource('search', 20, 'white'),
+  ]).then((sources) => {
+    Navigation.startTabBasedApp({
+      tabs: [
+        {
+          screen: 'sell_it_app.Home',
+          label: 'Home',
+          title: 'Home',
+          icon: sources[2],
+        },
+        {
+          screen: 'sell_it_app.AddPost',
+          label: 'Sell it',
+          title: 'Sell it',
+          icon: sources[1],
+        },
+      ],
+    });
   });
 };
 
