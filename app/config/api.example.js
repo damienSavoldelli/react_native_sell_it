9b8048aa97f22004074e79070671227267bac3c0
damien <damien.savoldelli@gmail.com>
2018-08-26 21:34:24 +0200

    create firebase api config example

---
+++ app/config/api.example.js
@@ -0,0 +1,5 @@
+export const FIREBASEURL = '[URL_FIREBASE]';
+export const APIKEY = '[API_KEY_FIREBASE]';
+export const SIGNUP = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${APIKEY}`;
+export const SIGNIN = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${APIKEY}`;
+export const REFRESH = `https://securetoken.googleapis.com/v1/token?key=${APIKEY}`;
