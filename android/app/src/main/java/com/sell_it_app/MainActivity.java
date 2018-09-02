43bc8eaf70cdab55c490b9f1a990c459662b9e8f
damien <damien.savoldelli@gmail.com>
2018-09-02 23:49:02 +0200

    create launch screen for android

--- android/app/src/main/java/com/sell_it_app/MainActivity.java
+++ android/app/src/main/java/com/sell_it_app/MainActivity.java
@@ -1,7 +1,29 @@
 package com.sell_it_app;
 
 import com.reactnativenavigation.controllers.SplashActivity;
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.util.TypedValue;
+import android.widget.TextView;
 
 public class MainActivity extends SplashActivity {
+
+    @Override
+    public LinearLayout createSplashLayout(){
+        LinearLayout view = new LinearLayout(this);
+        TextView text = new TextView(this);
+
+        view.setBackgroundColor(Color.parseColor("#00ADA9"));
+        view.setGravity(Gravity.CENTER);
+
+        text.setText("Sell It App");
+        text.setTextColor(Color.parseColor("#ffffff"));
+        text.setGravity(Gravity.CENTER);
+        text.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 60);
+
+        view.addView(text);
+        return view;
+    }
 
 }
